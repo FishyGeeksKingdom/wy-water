225f3daa01b3e52220e6e66176838605d255177d
Dustin Kovac <dustin.kovac@callawaycloudconsulting.com>
2022-03-10 16:09:30 -0700

    - pushing

---
+++ index.js
@@ -0,0 +1,18 @@
+module.exports = async function getSnakeRiverFlows() {
+  const uri =
+    "http://waterservices.usgs.gov/nwis/iv/?site=13022500&format=json";
+
+  const xhr = new XMLHttpRequest();
+
+  xhr.open("GET", uri);
+
+  xhr.onload = () => {
+    if (xhr.status === 200) {
+      data = JSON.parse(xhr.responseText);
+      console.log(data);
+      return data;
+    } else {
+      console.log("Something went wrong!!");
+    }
+  };
+};
