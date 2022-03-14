b07c1570f71c3c2e5cb2325a71e24777b79b4565
Dustin Kovac <dustin.kovac@callawaycloudconsulting.com>
2022-03-14 15:47:53 -0600

    - hmmm

--- checkReviews.py
+++ checkReviews.py
@@ -7,7 +7,7 @@ if __name__== "__main__":
   reviews_raw = sys.argv[1]
   teams_raw = sys.argv[2]
 
-  reviews = json.loads(reviews_raw)["data"]["organization"]["repository"]["reviews"]["edges"]
+  reviews = json.loads(reviews_raw)["data"]["organization"]["repository"]
   teams = json.loads(teams_raw)
 
   print(reviews)
