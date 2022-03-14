273361282057bf87815b43698f73b88c22115b3d
Dustin Kovac <dustin.kovac@callawaycloudconsulting.com>
2022-03-14 15:12:37 -0600

    - this might actually work

--- checkReviews.py
+++ checkReviews.py
@@ -1,3 +1,14 @@
 
+import sys
+import json
+
 if __name__== "__main__":
+
+  reviews = sys.argv[1]
+  teams = sys.argv[2]
+
+  print(reviews)
+  print(teams)
+
+
   print("Hello World from the Devops repo!")
